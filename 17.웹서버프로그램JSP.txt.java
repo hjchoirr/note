8fd801e19be4c0af181be83bc48d41eb0c572b73
hjchoirr <hjchoir@naver.com>
2024-06-13 17:08:22 +0900

    6/13

--- 17.웹서버프로그램JSP.txt.java
+++ 17.웹서버프로그램JSP.txt.java
@@ -867,14 +867,14 @@
 
 JSP의 특징
 (Java Server Page) - 서블릿 코드 번역기술
-1. JSP는 서블릿 기술의 확장
-2. JSP는 유지 관리가 용이
-3. JSP는 빠른 개발이 가능
-4. JSP로 개발하면 코드 길이를 줄일 수 있다.
+	1. JSP는 서블릿 기술의 확장
+	2. JSP는 유지 관리가 용이
+	3. JSP는 빠른 개발이 가능
+	4. JSP로 개발하면 코드 길이를 줄일 수 있다.
 
 JSP의 페이지 처리과정
 	
-	post.jsp -> post_jsp.java -> post_jsp.class -> 실행 -> _jspInit() 한번 -> _jspService() 매 요청시 -> _jspDestroy() 실행
+	post.jsp -> post_jsp.java -> 컴파일 -> post_jsp.class -> 실행 -> _jspInit() - 한번 -> _jspService() - 매 요청시 -> _jspDestroy();
 	
 	Catalina base : C:\Users\admin\.SmartTomcat\day04\day04.main 내의 work 폴더
 	
@@ -892,18 +892,30 @@ JSP 생명 주기
 	참고)
 		_jspService 메서드 지역 내부에 정의된 객체 -> 내장 객체 
 		-> jsp 페이지에서 바로 접근이 가능 
-			
-			HttpServletRequest request : 요청 관련 정보, 기능 
-			HttpServletResponse response : 응답 관련 정보, 기능
 		
-			PageContext pageContext : JSP로 번역된 서블릿 클래스의 환경 정보, 기능
-			ServletContext application : 서블릿(애플리케이션) 환경 정보, 기능
-			ServletConfig config : 서블릿 설정 
-			HttpSession session : 세션 
-			JspWriter out : JSP 페이지에 출력
-			Object page = this // 생성된 서블릿 객체를 참조 
-			
+			- 외우기
+			1)HttpServletRequest request : 요청 관련 정보, 기능 
+			2)HttpServletResponse response : 응답 관련 정보, 기능
 		
+			3)PageContext pageContext : JSP로 번역된 서블릿 클래스의 환경 정보, 기능
+			4)ServletContext application : 서블릿(애플리케이션) 환경 정보, 기능
+			5)ServletConfig config : 서블릿 설정 
+			6)HttpSession session : 세션 
+			7)JspWriter out : JSP 페이지에 출력
+			8)Object page = this // 생성된 서블릿 객체를 참조 
+			
+			9)Throwable exception   // 에러 페이지에서
+			
+			----------------------------------------------------------------------
+			try {
+				response.setContentType("text/html; charset=UTF-8");
+				pageContext = _jspxFactory.getPageContext(this, request, response, null, true, 8192, true);
+				application = pageContext.getServletContext();
+				config = pageContext.getServletConfig();
+				session = pageContext.getSession();
+				out = pageContext.getOut();
+			----------------------------------------------------------------------
+
 		jsp 페이지에서 자바 코드 : 번역시 자바 코드 그자체로 추가 
 		<%
 			자바 코드 
@@ -952,7 +964,7 @@ JSP 생명 주기
 				System.out.println(result);
 				
 			%>
-
+			<%=num1%>+<%=num2%>=<%=result%>
 			<%! 
 				// 선언문 : 인스턴스변수
 				
@@ -965,18 +977,57 @@ JSP 생명 주기
 				
 			%>
 			---------------------------------------------------
-	_jspService메서드 내부에 정의된 지역변수 : 내장 객체(jsp 페이지에서 바로 접근 가능한 객체
-		- 외우기
-		HttpServletRequest request : 요청관련 정보, 기능
-		HttpServletResponse response : 응답관련 정보, 기능
-		PageContext pageContext : JSP로 번역된 서블릿 클래스의 환경정보, 기능
-		ServletConfig config : 서블릿 설정
-		ServletContext application : 서블릿 환경정보, 기능
-		JspWriter out : JSP 페이지에 출력
-		Object page = this 생성된 서블릿 객체를 참조
-		HttpSession session : 세션
+			post_jsp.jsp
+			-------------------------------------------------
+			public final class post_jsp extends org.apache.jasper.runtime.HttpJspBase
+				implements org.apache.jasper.runtime.JspSourceDependent,
+							 org.apache.jasper.runtime.JspSourceImports,
+							 org.apache.jasper.runtime.JspSourceDirectives {
 
+				// 선언문 : 인스턴스변수
+				int num1 = 10;
+				int num2 = 20;
+				
+				int add(int num1, int num2) {
+					return num1 + num2;
+				}
+				...
 
+				public void _jspService(final jakarta.servlet.http.HttpServletRequest request, final jakarta.servlet.http.HttpServletResponse response)
+				throws java.io.IOException, jakarta.servlet.ServletException {
+						// ...
+						out.write("\r\n");
+						out.write("<h1>게시글 작성</h1>\r\n");
+						out.write("\r\n");
+						out.write("<form method=\"post\" action=\"post_ps.jsp\">\r\n");
+						out.write("    제목 : <input type=\"text\" name='subject'><br>\r\n");
+						out.write("    내용 : <textarea name=\"content\"></textarea><br>\r\n");
+						out.write("    <button type=\"submit\">작성하기</button>\r\n");
+						out.write("</form>\r\n");
+
+						// 스크립틀릿 : 메서드 지역 내부 _jspService() 
+						int num1 = 100;
+						int num2 = 200;
+
+						//int result = num1 + num2;
+
+						int result = add(num1, num2);
+						System.out.println(result);
+						
+						out.write("<br>\r\n");
+						out.print(num1);
+						out.write('+');
+						out.print(num2);
+						out.write('=');
+						out.print(result);
+
+					} catch (java.lang.Throwable t) {
+							...
+						}
+					} 
+				}	
+			-------------------------------------------------	
+6/13 동영상, day04 소스 exam02 or exam03 부터 ??
 디렉티브 태그
 	<%@ ..... %>
 	- page 
@@ -991,24 +1042,455 @@ JSP 생명 주기
 	- taglib 
 
 	1. page 디렉티브 태그의 기능과 사용법
+		language, contentType, pageEncoding, 
+		
+		<%@ page language="java" %>
+		<%@ page contentType="text/html" %>
+		<%@ page pageEncoding="UTF-8" %>
+
+
+		<%@ page contentType="text/html" pageEncoding="UTF-8" %>
+		또는
+		<%@ page contentType="text/html; charset=UTF-8" %>
+		
+		<%@ page import="java.util.*" %>
+		<%@ page import="java.time.LocalDateTime" %>
+		또는
+		<%@ page import="java.util.*,java.time.LocalDateTime" %>
+		
+		<%@ page session="true" %>
+
+		<%@ page buffer="32kb" %>: 기본값 8KB
+		
+		<%@ page autoFlush="true" %>  기본값 true : flush는 비운다 = 출력한다
+		
+		<%@ page info="Home Page JSP" %> 
+		
+		<%@ page errorPage="error.jsp" %> 현재 페이지에서 에러 나면 error.jsp 페이지로 가라
+		<%@ page isErrorPage="true" %>  현재 페이지가 오류페이지인지
+			---------------------------------------------------------
+			9.웹서버프로그램구현/day04/exam01/ex03.jsp
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<%@ page errorPage="error.jsp" %>
+			<%
+				int num1 = 0;
+				int num2 = 20;
+				
+				int result = num2 / num1;
+			%>
+
+			---------------------------------------------------------
+			error.jsp
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<%@ page isErrorPage="true" %>
+			<h1>에러 발생</h1>
+			<%=exception.getMessage()%>
+			<%
+				exception.printStackTrace(new java.io.PrintWriter(out));
+			%>
+			---------------------------------------------------------
+			==>	error_jsp.java
+			java.lang.Throwable exception = org.apache.jasper.runtime.JspRuntimeLibrary.getThrowable(request);
+			if (exception != null) {
+			  response.setStatus(jakarta.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			}
+			...
+			out.print(exception.getMessage());
+			exception.printStackTrace(new java.io.PrintWriter(out));
+			...
+					
+			---------------------------------------------------------
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			${10 + 20}    // <-- EL식
+
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<%@ page isELIgnored="true" %>
+			${10 + 20}
+			---------------------------------------------------------
+
+		
+		
 	2. include 디렉티브 태그의 기능과 사용법
+		-> JSP, 서블릿, 텍스트파일 출력 결과물을 물리적으로 선언
+		
 		file="jsp|HTML 경로"
 		<%@ include file="..." %>
 		
+			- ex01.jsp : 각각 출력결과 끼워짐 ( 코드가 아니라 )
+			---------------------------------------------------
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<%@ include file="header.jsp" %>
+			<main>
+				<h1>내용영역</h1>
+			</main>
+			<%@ include file="footer.jsp" %>
+			---------------------------------------------------
+			- ex01_jsp.java 
+			out.write("<!DOCTYPE html>\r\n");
+			out.write("<html>\r\n");
+			out.write("	<head>\r\n");
+			out.write("		<title>\r\n");
+			out.write("		사이트!\r\n");
+			out.write("		</title>\r\n");
+			out.write("	</head>\r\n");
+			out.write("	<body>\r\n");
+			out.write("		<header>\r\n");
+			out.write("			상단영역\r\n");
+			out.write("		</header>\r\n");
+			out.write("		\r\n");
+			out.write("\r\n");
+			out.write("<main>\r\n");
+			out.write("	<h1>내용영역</h1>\r\n");
+			out.write("</main>\r\n");
+			out.write("\r\n");
+			out.write("		<footer>\r\n");
+			out.write("			<h1>하단영역</h1>\r\n");
+			out.write("		</footer>\r\n");
+			out.write("	</body>\r\n");
+			out.write("</html>");
+		
 	3. taglib 디렉티브 태그의 기능과 사용법
 		- 태그 라이브러리 
+		
 		JSTL (JSP Standard Tag Libaray) - 3.0
 		uri="jakarta.tags.core"
+		
+		(참고) 예전 JSTL(1.2 ~)
+			uri = "http://java.sun.com/jsp/jstl/core|fmt|functions"
 		
 		- JSTL 3.0
 		
 			- jstl-api 
 			- jstl 구현체 
+			
+			
+			의존성 mvn repository -> 선택 주의 
+			Jakarta Standard Tag Library Implementation » 3.0.1
+			- api와 구현체 모두 포함되어 있음
+			
+			implementation 'org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.1'
+
+			
 		
 	JSP의 주석 처리
-	<%-- 주석 --%> : 번역 X
+		<%-- 주석 --%> : 번역 X
 
-	/*  */ : 자바 코드의 주석으로 번역
+		/*  */ : 자바 코드의 주석으로 번역
 
 
 	https://jakarta.ee/specifications/tags/3.0/tagdocs/
+
+액션태그
+	- 액션 태그는 서버나 클라이언트에게 어떤 행동을 하도록 명령하는 태그
+	- 스크립트 태그, 주석, 디렉티브 태그와 함게 JSP 페이지를 구성하는 태그
+	- 커스텀 태그의 일종
+
+
+	액션태그의 종류
+		1. forward 액션 태그의 기능과 사용법
+			<jsp:forward page="이동할 페이지 - JSP, html, text, servlet 경로"  />
+			 - 페이지 이동 / 버퍼의 통제 
+			 - 기존 출력 버퍼를 취소, page에 정의된 경로의 출력 결과물을 버퍼에 출력하고 비운다.
+			 - location 아님 (중요), 버퍼를 이용한 방식 
+			 - 출력 결과물을 담아 교체함, 페이지는 이동하지 않고 출력 버퍼내용만 치환 
+			 - 아래 ex01.jsp 과 ex03.jsp 의 차이
+			 - ex01.jsp 가 8kb 이상이면 그 다음에 치환됨 -- /exam03/ex01.jsp 
+				-------------------------------------------------			 
+				ex01.jsp
+				<%@ page contentType="text/html;charset=UTF-8" %>
+				<h1>ex01.jsp</h1>
+				<jsp:forward page="ex02.jsp" />
+				-------------------------------------------------			 
+				ex02.jsp
+				<%@ page contentType="text/html;charset=UTF-8" %>
+				<h1>ex02.jsp</h1>
+				-------------------------------------------------	
+				<%@ page contentType="text/html;charset=UTF-8" %>
+				<h1>ex03.jsp</h1>
+				<%
+					response.sendRedirect("ex02.jsp");
+				%>
+				-------------------------------------------------	
+
+		2. include 액션 태그의 기능과 사용법
+		
+			- 페이지 추가 / 버퍼의 통제 - 버퍼 내용 추가 방식
+			
+			-------------------------------------------------	
+			9.웹서버프로그램구현\day04\src\main\webapp\exam04\ex01.jsp
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<jsp:include page="header.jsp" />
+			<main>
+				<h1>내용영역...</h1>
+			</main>
+			<jsp:include page="footer.jsp" />
+			-------------------------------------------------	
+			ex01_jsp.java
+			out.write('\r');
+			out.write('\n');
+			org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "header.jsp", out, false);
+			out.write("\r\n");
+			out.write("<main>\r\n");
+			out.write("	<h1>내용영역...</h1>\r\n");
+			out.write("</main>\r\n");
+			org.apache.jasper.runtime.JspRuntimeLibrary.include(request, response, "footer.jsp", out, false);
+			out.write('\r');
+			out.write('\n');
+			-------------------------------------------------	
+			
+
+		3. include 액션 태그와 include 디렉티브 태그의 차이
+			- include 디렉티브 : 물리적인 결과물이 추가된채로 하나의 java, class 파일 만들어짐
+			- include 액션 태그 : 각각의 jsp에 대한 각각의 java 파일 만들어짐, 출력버퍼 추가 방식
+			
+		*** 액션태그 forward, include => 출력 버퍼치환, 버퍼추가 
+		
+		(참고)RequestDispatcher() 메서드의 동작과 동일 - forward(..), include(..)
+			
+			- ex01.jsp
+			----------------------------------------------------
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<%@ page import="jakarta.servlet.*" %>
+			<h1>ex01.jsp - 상단</h1>
+			<%
+				RequestDispatcher rd = request.getRequestDispatcher("ex02.jsp");
+				//rd.forward(request, response);
+				rd.include(request, response);
+			%>
+			<h1>ex01.jsp - 하단</h1>
+			
+			----------------------------------------------------
+			- ex02.jsp
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<h1>ex02.jsp</h1>
+			
+			----------------------------------------------------
+			
+			WEB-INF폴더 : 서블릿에서만 접근 가능하므로 뷰 템블릿jsp들을 여기에 넣어야 함. 
+			
+				------------------------------------------------------------------------
+				package order.controllers;
+				import jakarta.servlet.RequestDispatcher;
+				import jakarta.servlet.ServletException;
+				import jakarta.servlet.annotation.WebServlet;
+				import jakarta.servlet.http.HttpServlet;
+				import jakarta.servlet.http.HttpServletRequest;
+				import jakarta.servlet.http.HttpServletResponse;
+				import java.io.IOException;
+
+				@WebServlet("/order")
+				public class OrderController extends HttpServlet {
+					@Override
+					public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+						//서비스 처리...
+						req.setAttribute("message", "처리완료");  // 뷰에 전달해줄 값
+
+						//출력처리 (View : order.jsp")
+						RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/order/order.jsp");
+						rd.forward(req, resp);
+					}
+				}
+				------------------------------------------------------------------------
+				- order.jsp
+				<%@ page contentType="text/html;charset=UTF-8" %>
+				<h1>주문서 확인</h1>
+				<h2>${message}</h2>
+				------------------------------------------------------------------------
+
+
+
+			
+		4. param 액션 태그의 기능과 사용법
+			- param 액션 태그는 현재 JSP 페이지에서 다른 페이지에 정보를 전달하는 태그
+			- 요청 데이터를 전달
+			- <jsp:forward /> <jsp:include />
+			   : 요청 처리 중에 버퍼추가 / 요청 처리 중 추가 정보를 전달
+			   : request.getParameter()
+			
+
+				-----------------------------------------------------
+				ex03.jsp(exam05)
+				<%@ page contentType="text/html;charset=UTF-8" %>
+				<h1>ex03.jsp</h1>
+				<jsp:forward page="ex04.jsp">
+					<jsp:param name="key1" value="value1" />
+					<jsp:param name="key2" value="value2" />
+				 </jsp:forward>
+				 
+				-----------------------------------------------------
+				ex04.jsp
+				<%@ page contentType="text/html;charset=UTF-8" %>
+				<h1>ex04.jsp</h>
+				<%
+				String key1 = request.getParameter("key1");
+				String key2 = request.getParameter("key2");
+				%>
+				<br>
+				key1 : <%=key1%>
+				<br>
+				key2 : <%=key2%>
+				-----------------------------------------------------
+
+
+		한글데이터 post submit 하고 크롬에서 헤더 보면
+		contentType : application/x-www-form-urlencoded
+
+			--------------------------------------------------------- 
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<%@ page import="java.net.*" %>
+			<h1>ex03.jsp</h1>
+			<%
+			String key1 = URLEncoder.encode("이름1", "UTF-8");
+			%>
+			<jsp:forward page="ex04.jsp">
+				<jsp:param name="key1" value="<%=key1%>" />
+				<jsp:param name="key2" value="value2" />
+			 </jsp:forward>
+			--------------------------------------------------------- 
+			<%@ page contentType="text/html;charset=UTF-8" %>
+			<%@ page import="java.net.*" %>
+			<h1>ex04.jsp</h>
+			<%
+			String key1 = URLDecoder.decode(request.getParameter("key1"),"UTF-8");
+			String key2 = request.getParameter("key2");
+			%>
+			<br>
+			key1 : <%=key1%>
+			<br>
+			key2 : <%=key2%>
+			--------------------------------------------------------- 
+
+
+	(참고)
+	 - 속성을 관리하는 객체 4가지
+	 - 속성 : 뷰에서 사용할 수 있는 데이터
+	
+		PageContext pageContext : 하나의 jsp 페이지 내에서만 조회
+		HttpServletRequest request : 하나의 요청이 처리되는 범위에서 조회 가능
+		HttpSession session : 세션이 유지되는 범위에서 조회가능
+		ServletContext application : 매플리케이션이 종료되기 전까지 조회 가능
+
+		void setAttribute(String name, Object value) : 속성지정 / 없을땐 추가, 있을땐 추가
+		Object getAttribute(String name) : 속성조회
+		void removeAttribute(String name) : 속성제거
+			
+		데이터가 유지되는 범위
+			pageContext < request < session < application
+
+		---------------------------------------------------------
+		ex01.jsp (exam06)
+		<%@ page contentType="text/html;charset=UTF-8" %>
+		<%
+			//pageContext.setAttribute("key1", "value1");
+			request.setAttribute("key1", "value1");
+		%>
+		<jsp:include page="ex02.jsp" />
+
+		<%
+			//String key1 = (String) pageContext.getAttribute("key1");
+			String key1 = (String) request.getAttribute("key1");
+		%>
+		<div>
+			ex01-key1: <%=key1%>
+		</div>
+
+		---------------------------------------------------------
+		ex02.jsp
+		<%@ page contentType="text/html;charset=UTF-8" %>
+		<%
+			//String key1 = (String) pageContext.getAttribute("key1");
+			String key1 = (String) request.getAttribute("key1");
+		%>
+
+		<div>
+			ex02-key1: <%=key1%>
+		</div>
+		---------------------------------------------------------
+
+		게시물 목록 예제
+
+		----------------------------------------------------------
+		/WEB-INF/templates/board/list.jsp
+		<%@ page contentType="text/html;charset=UTF-8" %>
+		<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+		<h1>게시글 목록</h1>
+		<c:forEach var="item" items="${items}" >
+			<li>
+				${item.subject} / ${item.poster} / ${item.content} / ${item.regDt} /
+			</li>
+		</c:forEach>
+		----------------------------------------------------------
+		package board.controllers;
+		import board.entities.BoardData;
+		import board.services.BoardInfoService;
+		import jakarta.servlet.RequestDispatcher;
+		import jakarta.servlet.ServletException;
+		import jakarta.servlet.annotation.WebServlet;
+		import jakarta.servlet.http.HttpServlet;
+		import jakarta.servlet.http.HttpServletRequest;
+		import jakarta.servlet.http.HttpServletResponse;
+		import java.io.IOException;
+		import java.util.List;
+
+		@WebServlet("/board/list/*")
+		public class BoardListController extends HttpServlet {
+			@Override
+			public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+				BoardInfoService service = new BoardInfoService();
+				List<BoardData> items = service.getList();
+
+				req.setAttribute("items", items);
+
+				RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/board/list.jsp");
+				rd.forward(req, resp);
+			}
+		}
+		----------------------------------------------------------
+
+		package board.entities;
+		import lombok.Builder;
+		import lombok.Data;
+		import java.time.LocalDateTime;
+
+		@Data
+		@Builder
+		public class BoardData {
+			private long seq;
+			private String subject;
+			private String content;
+			private String poster;
+			private LocalDateTime regDt;
+		}
+		----------------------------------------------------------
+
+		package board.services;
+		import board.entities.BoardData;
+		import java.time.LocalDateTime;
+		import java.util.List;
+		import java.util.stream.IntStream;
+
+		public class BoardInfoService {
+			public List<BoardData> getList() {
+				//기시글 목록 조회
+
+				List<BoardData> items = IntStream.rangeClosed(1, 10).mapToObj(i-> BoardData.builder()
+				.seq(i).subject("제목" + i)
+						.content("내용" + i)
+						.poster("작성자" + i)
+						.regDt(LocalDateTime.now())
+						.build()).toList();
+				return items;
+			}
+		}
+		----------------------------------------------------------
+
+
+
+		5. 자바빈즈 액션 태그의 기능과 사용법
+			1) 자바빈즈 :  데이터 표현을 목적으로 하는 자바 클래스
+			2) 자바빈즈 작성 규칙
+			3) useBean 액션 태그로 자바빈즈 사용하기
+			4) setProperty 액션 태그로 프로퍼티 값 저장하기
+			5) getProperty 액션 태그로 프로퍼티의 값 가져오기
